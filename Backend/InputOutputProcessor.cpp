8b9e356f6c52c675a8f8817b7b3bdf4d33b6dc1c
lolsniperftw <41841264+lolsniperftw@users.noreply.github.com>
2024-04-18 19:51:02 +0300

    Switch from ifstream to QFile and QTextStream

--- Backend/InputOutputProcessor.cpp
+++ Backend/InputOutputProcessor.cpp
@@ -4,82 +4,85 @@
 #include <iostream>
 #include <algorithm>
 #include <stdexcept>
+#include <QFile>
+#include <qdebug.h>
 
 namespace InputOutputProcessor
 {
-	const std::string raw_input_file_name = "Backend/wordsWithDefinitions.txt";
-	const std::string processed_input_file_name = "Backend/words.txt";
-	
-	/**
+    const std::string raw_input_file_name = "Backend/wordsWithDefinitions.txt";
+    const std::string processed_input_file_name = "Backend/words.txt";
+
+    /**
 	 * The original list of words contained their definitions, which is redundant for our purposes.
 	 * This function creates a new text file with a single word in every line.
 	 * Has no use if the original list is already modified.
 	 */
-	void remove_definitions()
-	{
-		std::ifstream input_file(raw_input_file_name);
+    void remove_definitions()
+    {
+        std::ifstream input_file(raw_input_file_name);
 
-		if (!input_file.is_open())
-		{
-			throw std::invalid_argument("Cannot open " + raw_input_file_name + "!\n");
-		}
+        if (!input_file.is_open())
+        {
+            throw std::invalid_argument("Cannot open " + raw_input_file_name + "!\n");
+        }
 
-		std::ofstream output_file(processed_input_file_name);
-		std::string s;
+        std::ofstream output_file(processed_input_file_name);
+        std::string s;
 
-		while (getline(input_file, s))
-		{
-			for (const char c : s)
-			{
-				if (c == ' ') // end of the word, start of the definition
-				{
-					break;
-				}
-				else
-				{
-					output_file << static_cast<char>(std::tolower(c));
-				}
-			}
+        while (getline(input_file, s))
+        {
+            for (const char c : s)
+            {
+                if (c == ' ') // end of the word, start of the definition
+                {
+                    break;
+                }
+                else
+                {
+                    output_file << static_cast<char>(std::tolower(c));
+                }
+            }
 
-			output_file << "\n";
-		}
-	}
+            output_file << "\n";
+        }
+    }
 
-	std::vector<std::string> get_list_of_valid_words()
-	{
-		// remove_definitions(); // done beforehand
+    std::vector<std::string> get_list_of_valid_words()
+    {
+        // remove_definitions(); // done beforehand
 
-		std::ifstream input(processed_input_file_name);
+        QFile input(":/words.txt");
 
-		if (!input.is_open())
-		{
-			throw std::invalid_argument("Cannot open " + processed_input_file_name + "!\n");
-		}
+        if (!input.open(QIODevice::ReadOnly | QIODevice::Text))
+        {
+            throw std::invalid_argument("Cannot open " + input.fileName().toStdString() + "!\n");
+        }
 
-		std::string s;
-		std::vector<std::string> words;
+        std::vector<std::string> words;
+        QTextStream in(&input);
 
-		while (getline(input, s))
-		{
-			if (s.length() >= Parameters::MIN_WORD_LENGTH)
-			{
-				words.push_back(s);
-			}
-		}
+        while(!in.atEnd())
+        {
+            QString line = in.readLine();
+            if (line.length() >= Parameters::MIN_WORD_LENGTH)
+            {
+                words.push_back(line.toStdString());
+            }
+        }
 
-		return words;
-	}
+        return words;
+    }
 
-	/**
+    /**
 	 * Reads the input.
 	 *
 	 * Currently the input is hardcoded.
 	 *
 	 * @return a 2d grid filled with letters.
 	 */
-	std::vector<std::vector<char>> process_input()
-	{
-		/* Example of an input with empty cells:
+    std::vector<std::vector<char>> process_input()
+    {
+        /* Example of an input with empty cells:
 
 		const char XXX = Parameters::EMPTY_CELL;
 		const std::vector<std::vector<char>> grid{
@@ -90,32 +93,32 @@ namespace InputOutputProcessor
 													{XXX,'w','e','b',XXX}};
 		*/
 
-		// Squaredle of 15.4.2024
-		const std::vector<std::vector<char>> grid{{'n', 'm', 'a', 'b'},
-												  {'i', 'b', 'n', 'g'},
-												  {'a', 'u', 'e', 'n'},
-												  {'n', 'q', 'a', 't'}};
+        // Squaredle of 15.4.2024
+        const std::vector<std::vector<char>> grid{{'n', 'm', 'a', 'b'},
+                                                  {'i', 'b', 'n', 'g'},
+                                                  {'a', 'u', 'e', 'n'},
+                                                  {'n', 'q', 'a', 't'}};
 
-		if (std::any_of(grid.begin(), grid.end(), [&](const auto& row) { return row.size() != grid.size(); }))
-		{
-			throw std::invalid_argument("Grid must be square!\n");
-		}
+        if (std::any_of(grid.begin(), grid.end(), [&](const auto& row) { return row.size() != grid.size(); }))
+        {
+            throw std::invalid_argument("Grid must be square!\n");
+        }
 
-		return grid;
-	}
+        return grid;
+    }
 
-	/**
+    /**
 	 * Presents the output in the desired manner.
 	 *
 	 * Prints the words found in the grid, ordered by their length.
 	 *
 	 * @param found_words The words occurring in the input.
 	 */
-	void process_output(std::vector<std::string>& found_words)
-	{
-		// Sort the words by length (tie break by lexicographic order) and remove duplicates.
-		std::sort(found_words.begin(), found_words.end(), [](const std::string& word1, const std::string& word2)
-		{
+    void process_output(std::vector<std::string>& found_words)
+    {
+        // Sort the words by length (tie break by lexicographic order) and remove duplicates.
+        std::sort(found_words.begin(), found_words.end(), [](const std::string& word1, const std::string& word2)
+        {
             if (word1.length() != word2.length())
             {
                 return word1.length() < word2.length();
@@ -123,24 +126,24 @@ namespace InputOutputProcessor
             else
             {
                 return word1 < word2; // by lexicographic order
-            } 
-		});
+            }
+        });
 
-		found_words.erase(std::unique(found_words.begin(), found_words.end()), found_words.end()); // remove duplicates
+        found_words.erase(std::unique(found_words.begin(), found_words.end()), found_words.end()); // remove duplicates
 
-		// Print the results.
-		std::cout << "Found " << found_words.size() << " words!\n";
+        // Print the results.
+        std::cout << "Found " << found_words.size() << " words!\n";
 
-		for (size_t i = 0, previous_word_length = 0, word_index = 0; i < found_words.size(); ++i, ++word_index)
-		{
-			if (found_words[i].length() != previous_word_length)
-			{
-				std::cout << "\nwords with " << found_words[i].length() << " letters:\n";
-				previous_word_length = found_words[i].length();
-				word_index = 1;
-			}
+        for (size_t i = 0, previous_word_length = 0, word_index = 0; i < found_words.size(); ++i, ++word_index)
+        {
+            if (found_words[i].length() != previous_word_length)
+            {
+                std::cout << "\nwords with " << found_words[i].length() << " letters:\n";
+                previous_word_length = found_words[i].length();
+                word_index = 1;
+            }
 
-			std::cout << word_index << ") " << found_words[i] << "\n";
-		}
-	}
+            std::cout << word_index << ") " << found_words[i] << "\n";
+        }
+    }
 }
