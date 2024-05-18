f232fdde848fee9d94dc27db7ac0c994c2081465
lolsniperftw <41841264+lolsniperftw@users.noreply.github.com>
2024-05-18 14:56:25 +0300

    Remove commented code

--- Frontend/Squaredle-Solver/main.cpp
+++ Frontend/Squaredle-Solver/main.cpp
@@ -1,17 +1,9 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-// #include "../../Backend/Algorithm.hpp"
-// #include "../../Backend/InputOutputProcessor.hpp"
-// #include "gridmodel.h"
-
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-
-    // const std::vector<std::vector<char>> grid = InputOutputProcessor::process_input();
-    // std::vector<std::string> found_words = Algorithm::find_words(grid);
-    // InputOutputProcessor::process_output(found_words);
 
     QQmlApplicationEngine engine;
     QObject::connect(
