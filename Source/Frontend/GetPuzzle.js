bf130d480977374425a89934cf3b231cd81dcee7
lolsniperftw <41841264+lolsniperftw@users.noreply.github.com>
2024-12-04 17:23:55 +0200

    Implemented support for getting puzzle by id.

---
+++ Source/Frontend/GetPuzzle.js
@@ -0,0 +1,21 @@
+function getPuzzleById(id) {
+    const xhr = new XMLHttpRequest()
+    xhr.open('POST', 'https://cloudflare-cors-anywhere.oranhero.workers.dev/?https://squaredle.app/api/today-puzzle-config.js', true)
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const responseData = JSON.parse(xhr.response)
+            const puzzleData = responseData.data
+            if (puzzleData)
+                mainModule.gridModel.fillGrid(puzzleData.puzzle.board)
+            else
+                errorPopup.open()
+        }
+    }
+    xhr.send(JSON.stringify({
+        "args": {
+            "game": "squaredle",
+            "puzzleKey": id
+        },
+        "op": "getPuzzleByKey"
+    }))
+}
