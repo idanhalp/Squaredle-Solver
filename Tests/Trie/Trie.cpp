ed6e3d2d085f30ae34d4f0888fc2cc90999a5788
idanhalp <idanhal27@gmail.com>
2024-12-06 18:35:34 +0200

    Added tests to `Trie`.

---
+++ Tests/Trie/Trie.cpp
@@ -0,0 +1,31 @@
+#include "Source/Backend/Trie.hpp"
+#include "Tests/Trie/Trie.hpp"
+
+auto TrieTests::test_trie() const -> void
+{
+	const Trie trie({"ab", "aaa", "ba", "b"});
+
+	QVERIFY(trie.check_if_prefix_exists(""));
+	QVERIFY(trie.check_if_prefix_exists("aaa"));
+	QVERIFY(trie.check_if_prefix_exists("a"));
+	QVERIFY(trie.check_if_prefix_exists("b"));
+	QVERIFY(trie.check_if_prefix_exists("ba"));
+
+	QVERIFY(trie.check_if_complete_word_exists("ab"));
+	QVERIFY(trie.check_if_complete_word_exists("aaa"));
+	QVERIFY(trie.check_if_complete_word_exists("ba"));
+	QVERIFY(trie.check_if_complete_word_exists("b"));
+	QVERIFY(!trie.check_if_complete_word_exists("a"));
+	QVERIFY(!trie.check_if_complete_word_exists("aa"));
+	QVERIFY(!trie.check_if_complete_word_exists("aaaa"));
+	QVERIFY(!trie.check_if_complete_word_exists(""));
+	QVERIFY(!trie.check_if_complete_word_exists("baz"));
+}
+
+auto TrieTests::test_empty_trie() const -> void
+{
+	const Trie trie({});
+
+	QVERIFY(trie.check_if_prefix_exists(""));
+	QVERIFY(!trie.check_if_complete_word_exists(""));
+}
